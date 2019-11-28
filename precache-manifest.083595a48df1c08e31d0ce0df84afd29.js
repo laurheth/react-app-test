7951938924308b5294dfcc036c5c837af3430c10
Lauren Hetherington <laurenlhetherington@gmail.com>
2019-11-28 10:51:32 -0500

    Updates

---
+++ precache-manifest.083595a48df1c08e31d0ce0df84afd29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de679e25dd6c98e81dc65bb630cecba4",
+    "url": "/react-app-test/index.html"
+  },
+  {
+    "revision": "a84dcece2734bcb91dcd",
+    "url": "/react-app-test/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "581820e7a50d5bbfff9f",
+    "url": "/react-app-test/static/js/2.871a4e62.chunk.js"
+  },
+  {
+    "revision": "a84dcece2734bcb91dcd",
+    "url": "/react-app-test/static/js/main.57d03fdc.chunk.js"
+  },
+  {
+    "revision": "a44ff18868b73a304929",
+    "url": "/react-app-test/static/js/runtime-main.e5a96c93.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-app-test/static/media/logo.25bf045c.svg"
+  }
+]);
